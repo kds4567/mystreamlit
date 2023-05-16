6af903d3252298ffeea9e0e369953918fbb6b304
kds4567 <126741447+kds4567@users.noreply.github.com>
2023-05-16 12:48:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import numpy as np
+import panda as pd
+
+st.title("Map")
+df=pd.DataFrame(np.random.randn(500,2)/[50,50]+[37.26,-122.4],columns=['lat','lon'])
+st.map(df)
 
 x=st.slider('select a value')
 st.write(x,'squared is',x*x)
